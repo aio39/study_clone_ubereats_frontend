589f5dd9c0d9a250d51ce05ff0962d24456d7dbb
Aio <AioDev.JS@gmail.com>
2021-06-29 15:19:19 +0900

    GH-1 15.0 useReactiveVar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { gql, useQuery, useReactiveVar } from '@apollo/client';
 import React from 'react';
+import { isLoggedInVar } from './apollo';
 import { LoggedOutRouter } from './routers/logged-out-router';
+import { LoggedInRouter } from './routers/logged-in-router';
+
+// @client local state 사용
+// const IS_LOGGED_IN = gql`
+//   query isLoggedIn {
+//     isLoggedIn @client
+//   }
+// `;
 
 function App() {
-  return <LoggedOutRouter />;
+  // const {
+  //   data: { isLoggedIn },
+  // } = useQuery(IS_LOGGED_IN);
+
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
 
 export default App;
