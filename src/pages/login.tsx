200604c42d8d81e5dc8a2f88e1c3ff0d7aa0cabc
Aio <AioDev.JS@gmail.com>
2021-07-05 19:44:03 +0900

    GH-1 Create Account Mutation

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,6 +9,7 @@ import {
   loginMutation,
   loginMutationVariables,
 } from '../__generated__/loginMutation';
+import Helmet from 'react-helmet';
 
 const LOGIN_MUTATION = gql`
   mutation loginMutation($loginInput: LoginInput!) {
@@ -59,6 +60,9 @@ export const Login = () => {
   };
   return (
     <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <Helmet>
+        <title>Login | Nuber Eats</title>
+      </Helmet>
       <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
         <img src={nuberLogo} className="w-52 mb-10" />
         <h4 className="w-full font-medium text-left text-3xl mb-5">
